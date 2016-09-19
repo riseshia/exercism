31e7430770e1881489dd9d3a931d0dffce743b2e
Shia <rise.shia@gmail.com>
2016-09-19 11:27:18 +0900

    Improve ruby rna trnascription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,19 +1,17 @@
 module Complement
-  module_function
-
-  RNA_TO_DNA = {
-    "G" => "C",
-    "C" => "G",
-    "T" => "A",
-    "A" => "U"
-  }.freeze
-
-  def of_dna(strand)
-    dna?(strand) ? translate(strand) : ""
-  end
-
   class << self
+    def of_dna(strand)
+      dna?(strand) ? translate(strand) : ""
+    end
+
     private
+
+    RNA_TO_DNA = {
+      "G" => "C",
+      "C" => "G",
+      "T" => "A",
+      "A" => "U"
+    }.freeze
 
     def translate(strand)
       strand.each_char.map { |char| RNA_TO_DNA[char] }.join
