8784ba0a921510fbc7acc2247830e8a18f9f0e99
Shia <rise.shia@gmail.com>
2016-10-12 11:18:19 +0900

    javascript - bob

---
+++ javascript/bob/bob.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class Bob {
+  hey(msg) {
+    if (msg.trim() === "") {
+      return "Fine. Be that way!"
+    } else if (msg.toLowerCase() !== msg &&
+               msg.toUpperCase() === msg) {
+      return "Whoa, chill out!"
+    } else if (msg.endsWith("?")) {
+      return "Sure."
+    }
+    return "Whatever."
+  }
+}
+
+module.exports = Bob
