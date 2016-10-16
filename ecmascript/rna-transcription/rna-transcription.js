a2efd3b76c44f448c589d1ea189793b21763ba06
Shia <rise.shia@gmail.com>
2016-10-16 11:19:14 +0900

    Improve ecmascript - rna transcription

--- ecmascript/rna-transcription/rna-transcription.js
+++ ecmascript/rna-transcription/rna-transcription.js
@@ -2,7 +2,7 @@ const translation = {C: "G", G: "C", A: "U", T: "A"}
 
 class Transcriptor {
   toRna(dna) {
-    return dna.split("").map(one => translation[one]).join("")
+    return [...dna].map(one => translation[one]).join("")
   }
 }
 
