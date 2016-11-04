e66f470b9e9f0f4bf45b531ac6efa9a72ff15ed7
Shia <sim.sangyong@interfirm.co.jp>
2016-11-04 16:44:50 +0900

    Ecmascript - food chain

---
+++ ecmascript/food-chain/food-chain.js
@@ -0,0 +1,76 @@
+class Song {
+  verses(start, end) {
+    const verses = []
+    for(let i = start; i <= end; i++) {
+      verses.push(this.verse(i))
+    }
+    verses.push("")
+    return verses.join("\n")
+  }
+
+  verse(num) {
+    switch (num) {
+      case 1:
+        return `I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+`
+      case 2:
+        return `I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+`
+      case 3:
+        return `I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+`
+      case 4:
+        return `I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+`
+      case 5:
+        return `I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+`
+      case 6:
+        return `I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+`
+      case 7:
+        return `I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+`
+      case 8:
+        return `I know an old lady who swallowed a horse.
+She's dead, of course!
+`
+    }
+  }
+}
+
+export default Song
