a32a9032bd81db1ed8620f1746ec5c4c22cdccb4
Shia <rise.shia@gmail.com>
2016-11-21 12:55:16 +0900

    Update elixir - frequency

--- elixir/parallel-letter-frequency/frequency.exs
+++ elixir/parallel-letter-frequency/frequency.exs
@@ -9,15 +9,42 @@ defmodule Frequency do
   @spec frequency([String.t], pos_integer) :: map
   def frequency(texts, workers) do
     texts
-    |> Enum.flat_map(fn(text) ->
-      text
-      |> String.downcase
-      |> String.split("", trim: true)
-      |> Enum.reject(&(Regex.match?(~r/[\s,1-9]/, &1)))
-    end)
+    |> map_texts(workers)
+    |> Enum.map(fn(texts) -> Task.async(Frequency, :count_all, [texts]) end)
+    |> Enum.map(&Task.await/1)
+    |> Enum.reduce(%{}, &merge_map_with_plus/2)
+  end
+
+  def count_all(texts) do
+    texts
+    |> Enum.map(&count/1)
+    |> Enum.reduce(%{}, &merge_map_with_plus/2)
+  end
+
+  defp count(text) do
+    text
+    |> cleaning
     |> Enum.reduce(%{}, fn(word, acc) ->
-      value = Map.get(acc, word, 0)
-      Map.put(acc, word, value + 1)
+      value = Map.get(acc, word, 0) + 1
+      Map.put(acc, word, value)
     end)
+  end
+
+  defp merge_map_with_plus(map1, map2) do
+    Map.merge(map1, map2, fn(_key, v1, v2) -> v1 + v2 end)
+  end
+
+  defp cleaning(text) do
+    text
+    |> String.downcase
+    |> String.split("", trim: true)
+    |> Enum.reject(&(Regex.match?(~r/[\s,1-9]/, &1)))
+  end
+
+  defp map_texts(texts, workers) do
+    0..(workers-1)
+    |> Enum.map(
+      &(Enum.drop(texts, &1) |> Enum.take_every(workers))
+    )
   end
 end
