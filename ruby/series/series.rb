38f2f0a2e65c8cc9099fb642cff52d8f8c629d75
Shia <rise.shia@gmail.com>
2017-01-05 19:52:45 +0900

    Improve ruby - series

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,15 +1,12 @@
 class Series
-  attr_reader :num_arr
-  def initialize(num_str)
-    @num_arr = num_str.split("").map(&:to_i)
+  attr_reader :digits
+
+  def initialize(number)
+    @digits = number.chars.map(&:to_i)
   end
 
-  def slices(num)
-    raise ArgumentError, "It's too large" if num_arr.size < num
-
-    last_idx = num_arr.size - num
-    (0..last_idx).map do |idx|
-      num_arr[idx, num]
-    end
+  def slices(slice_size)
+    raise ArgumentError, "It's too large" if digits.size < slice_size
+    digits.each_cons(slice_size).to_a
   end
 end
