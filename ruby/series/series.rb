5e057bb2ce8ec653fe179b4e1b1e098d708f3215
Shia <rise.shia@gmail.com>
2017-01-06 08:15:13 +0900

    Improve ruby - series

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,12 +1,15 @@
 class Series
-  attr_reader :digits
-
   def initialize(number)
     @digits = number.chars.map(&:to_i)
   end
 
   def slices(slice_size)
-    raise ArgumentError, "It's too large" if digits.size < slice_size
+    raise ArgumentError, "Passed slice size is too large" \
+      if digits.size < slice_size
     digits.each_cons(slice_size).to_a
   end
+    
+  private
+
+  attr_reader :digits
 end
