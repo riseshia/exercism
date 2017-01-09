dc78f5b8c67e1e9636cb106568a5b9ba334ab189
Shia <rise.shia@gmail.com>
2017-01-09 17:28:33 +0900

    Improve python - bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,27 +2,11 @@
 # Skeleton file for the Python "Bob" exercise.
 #
 
-
-def is_shouting(what):
-    upper_case = what.upper()
-    if upper_case == what.lower():
-        return False
-    elif upper_case != what:
-        return False
-    else:
-        return True
-
-def is_question(what):
-    if what.endswith('?'):
-        return True
-    else:
-        return False
-
 def hey(what):
     what = what.strip()
-    if what == '':
+    if len(what) == 0:
         return 'Fine. Be that way!'
-    elif is_shouting(what):
+    elif what.isupper():
         return 'Whoa, chill out!'
     elif what.endswith('?'):
         return 'Sure.'
