b73e233a29d45e9964bebcde0890fddf13170df3
Shia <rise.shia@gmail.com>
2017-01-12 18:38:28 +0900

    Improve python - rna transcription

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,7 +1,7 @@
 dic = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
 
 def to_rna(dna):
-    rna = ''.join([dic.get(c, '') for c in dna])
+    rna = ''.join([dic.get(c, '') for c in dna if c in dic])
 
     if len(rna) == len(dna):
         return rna
