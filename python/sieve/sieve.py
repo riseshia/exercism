331fc6a9b01a83648e8cda64257580d1c054c3a7
Shia <rise.shia@gmail.com>
2017-01-13 12:27:39 +0900

    Python - Use the Sieve of Eratosthenes

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,15 +1,10 @@
-def _is_prime(n, primes):
-    for p in primes:
-        if n % p == 0:
-            return False
-    return True
-
 def sieve(number):
     primes = []
-    candidates = [2] + list(range(3, number + 1, 2))
+    candidates = range(2, number + 1)
 
-    for n in candidates:
-        if _is_prime(n, primes):
-            primes.append(n)
+    while len(candidates) > 0:
+        prime = candidates[0]
+        primes.append(prime)
+        candidates = [n for n in candidates[1:] if n % prime != 0]
 
     return primes
