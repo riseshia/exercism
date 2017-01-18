5237d92f93dc56dba65c24043cff31164c217cad
Shia <rise.shia@gmail.com>
2017-01-18 22:07:57 +0900

    Improve python - space age

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,30 +1,20 @@
 class SpaceAge:
     EARTH_SECOND = 365.25 * 24 * 60 * 60
+    table = {
+        'on_earth': 1,
+        'on_mercury': 0.2408467,
+        'on_venus': 0.61519726,
+        'on_mars': 1.8808158,
+        'on_jupiter': 11.862615,
+        'on_saturn': 29.447498,
+        'on_uranus': 84.016846,
+        'on_neptune': 164.79132,
+    }
 
     def __init__(self, seconds):
         self.seconds = seconds
         self.seconds_on_earth = seconds / self.EARTH_SECOND
 
-    def on_earth(self):
-        return round(self.seconds_on_earth, 2)
-
-    def on_mercury(self):
-        return round(self.seconds_on_earth / 0.2408467, 2)
-
-    def on_venus(self):
-        return round(self.seconds_on_earth / 0.61519726, 2)
-
-    def on_mars(self):
-        return round(self.seconds_on_earth / 1.8808158, 2)
-
-    def on_jupiter(self):
-        return round(self.seconds_on_earth / 11.862615, 2)
-
-    def on_saturn(self):
-        return round(self.seconds_on_earth / 29.447498, 2)
-
-    def on_uranus(self):
-        return round(self.seconds_on_earth / 84.016846, 2)
-
-    def on_neptune(self):
-        return round(self.seconds_on_earth / 164.79132, 2)
+    def __getattr__(self, name):
+        if name in self.table:
+            return lambda: round(self.seconds_on_earth / self.table[name], 2)
