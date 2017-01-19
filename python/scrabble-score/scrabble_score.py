16d9b8decf3403534f89ba5ce825f6546740c360
Shia <rise.shia@gmail.com>
2017-01-19 12:34:45 +0900

    Python - scrabble score

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,17 @@
+from string import ascii_lowercase
+
+score_table = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1,
+    'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8,
+    'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1,
+    'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1,
+    'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4,
+    'z': 10,
+}
+
+def score(string):
+    chars = [c for c in string.lower() if c in ascii_lowercase]
+    if len(chars) != len(string) or len(chars) == 0:
+        return 0
+
+    return sum(score_table[c] for c in chars)
