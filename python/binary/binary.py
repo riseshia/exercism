acb672bc553428a1c183f568a44a32e895871f68
Shia <rise.shia@gmail.com>
2017-01-26 12:30:20 +0900

    Improve python - binary

--- python/binary/binary.py
+++ python/binary/binary.py
@@ -1,2 +1,13 @@
 def parse_binary(bin_str):
-    return int(bin_str, 2)
+    acc = 0
+
+    for d in bin_str:
+        acc <<= 1
+        if d in '1':
+            acc += 1
+        elif d in '0':
+            pass
+        else:
+            raise ValueError('this is not valid binary string')
+
+    return acc
