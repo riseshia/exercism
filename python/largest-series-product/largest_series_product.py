71aa27d667fc6ed17e7a99dc547633a6d1617e56
Shia <rise.shia@gmail.com>
2017-03-07 20:20:34 +0900

    Improve Python - Largest Series Product

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,15 +1,14 @@
 from functools import reduce
 
+
 def largest_product(digits, width):
     if len(digits) < width or width < 0:
         raise ValueError
-    elif len(digits) == 0 or width == 0:
-        return 1
     numbers = [int(d) for d in digits]
 
     max = 0
     for i in range(0, len(numbers) - width + 1):
-        product = reduce(lambda x, y: x * y, numbers[i:i+width])
+        product = reduce(lambda x, y: x * y, numbers[i:i+width], 1)
         if max < product:
             max = product
     return max
