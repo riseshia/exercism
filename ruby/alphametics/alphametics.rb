416829315b65b2b2fef882fea228725ca66fa7a4
Shia <rise.shia@gmail.com>
2017-05-11 08:43:19 +0900

    Ruby - Alphametics(improve readability)

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -1,49 +1,32 @@
-module Helper
-  module_function
-
-  def test_expr?(exprs, rem, allow_zero = true)
-    exprs = exprs.map { |val| val.to_i.to_s } if allow_zero
-    *left, right = exprs
-    template = "(#{left.join(' + ')}) % #{rem} == #{right} % #{rem}"
-    return false if template.match?(/(?:(\(|\s))0\d+/)
-
-    eval(template) ? true : false
-  end
-end
-
-module Alphametics
-  module_function
-
-  def solve(template)
-    left, right = template.split(" == ")
-    args = left.split(" + ")
-    max_length = (args + [right]).max_by { |word| word.size }.size
-    vars = template.gsub(/[\s+=]/, "").chars.uniq.sort
-    var_length = vars.size
-
-    possibilities = [Array.new(var_length)]
-    keys = nil
-
-    0.upto(max_length - 1) do |i|
-      possibilities = step_by_solve(
-        vars, args, right, i, max_length - 1, possibilities
-      )
-
-      return {} if possibilities.empty?
-    end
-
-    vars.zip(possibilities.first).to_h
+class CSP
+  def initialize(vars, range)
+    @vars = vars
+    @range = range
+    @candidates = [Array.new(vars.size)]
   end
 
-  def manipulate_possibilities(possibilities, full_vars, target_vars)
-    new_possibilities = possibilities
+  def add(constraint)
+    solve_cons(constraint)
+  end
+
+  def solve
+    return {} if @candidates.empty?
+    @vars.zip(@candidates.first).to_h
+  end
+
+  private
+
+  def manipulate_candidates(cons)
+    return if @candidates.empty?
+    target_vars = cons.gsub(/[^A-Z]+/, "").chars.uniq.sort
+
     target_vars.each do |var|
       tmp = []
-      idx = full_vars.index(var)
-      next if new_possibilities.first[idx]
+      idx = @vars.index(var)
+      next if @candidates.first[idx]
 
-      new_possibilities.each do |pos|
-        0.upto(9).each do |i|
+      @candidates.each do |pos|
+        @range.each do |i|
           if pos.index(i).nil?
             new_pos = pos.clone
             new_pos[idx] = i
@@ -51,31 +34,55 @@ module Alphametics
           end
         end
       end
-      new_possibilities = tmp
+      @candidates = tmp
     end
-    new_possibilities
   end
 
-  def step_by_solve(vars, args, right, idx, last_idx, possibilities)
-    last_digit = idx == last_idx
+  def solve_cons(cons)
+    manipulate_candidates(cons)
 
-    target_left = args.map { |arg| arg.reverse[0..idx].reverse }
-    target_right = right.reverse[0..idx].reverse
-    target_vars = (target_left.join("") + target_right).chars.uniq.sort
-    possibilities = manipulate_possibilities(possibilities, vars, target_vars)
+    @candidates = @candidates.select do |candidate|
+      dic = @vars.zip(candidate).to_h
 
-    rem = 10 ** (idx + 1)
-
-    possibilities.select do |possibility|
-      dic = vars.zip(possibility).to_h
-
-      exprs = [*target_left, target_right].map do |el|
-        el.gsub(/[A-Z]/) do |matched|
-          dic[matched]
-        end
-      end
-      Helper.test_expr?(exprs, rem, !last_digit)
+      expr = cons.
+               gsub(/[A-Z]/) { |matched| dic[matched] }.
+               gsub(/[0-9]+/) { |matched| matched.to_i.to_s }
+      eval(expr)
     end
+  end
+end
+
+module Alphametics
+  module_function
+
+  def split_problem(template)
+    vars = template.split(/[\s+=]+/)
+    max_length = vars.max_by { |word| word.size }.size
+
+    cons = []
+    0.upto(max_length - 1) do |delta|
+      *left, right = vars.map { |var| var.reverse[0..delta].reverse }
+
+      rem = 10 ** (delta + 1)
+      con = "(#{left.join(' + ')}) % #{rem} == #{right} % #{rem}"
+      cons << con
+    end
+    cons
+  end
+
+  def solve(template)
+    vars = template.gsub(/[\s+=]/, "").chars.uniq.sort
+    csp = CSP.new(vars, (0..9).to_a)
+
+    cons = split_problem(template)
+    cons.last.scan(/[A-Z]+/).each do |var|
+      csp.add("#{var[0]} != 0")
+    end
+
+    cons.each do |cons|
+      csp.add(cons)
+    end
+    csp.solve
   end
 end
 
